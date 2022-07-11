52685f5198c0ed835953b5d8c7240614025c6c60
brupadhyay <brupadhyay_b20@ee.vjti.ac.in>
2022-07-11 18:00:06 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,69 @@
+
+var btns = document.querySelectorAll(".drum");
+
+for(var i=0; i<btns.length; i++){
+    btns[i].addEventListener("click", function (){
+
+        var buttonInner = this.innerHTML;
+
+        makeSound(buttonInner);
+
+        buttonAnimation(buttonInner)
+
+    });
+}
+// document.querySelector("button").addEventListener("click",handleClick);
+
+//Detecting Keyboard press
+document.addEventListener("keydown", function(event){
+
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+});
+
+function makeSound(key){
+
+    switch(key){
+        case "w": var crash = new Audio("sounds/crash.mp3");
+                  crash.play();
+                  break;
+
+        case "a": var tom1 = new Audio("sounds/tom-1.mp3");
+                  tom1.play();
+                  break;
+        
+                  
+        case "s": var tom2 = new Audio("sounds/tom-2.mp3");
+                  tom2.play();
+                  break;
+
+        case "d": var tom3 = new Audio("sounds/tom-3.mp3");
+                  tom3.play();
+                  break;
+        
+        case "j": var tom4 = new Audio("sounds/tom-4.mp3");
+                  tom4.play();
+                  break;
+
+        case "k": var snare = new Audio("sounds/snare.mp3");
+                  snare.play();
+                  break;
+
+        case "l": var kick = new Audio("sounds/kick-bass.mp3");
+                  kick.play();
+                  break;
+
+        default:  console.log(buttonInner);
+    }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+ currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function (){
+        activeButton.classList.remove("pressed");
+    },100);
+}
